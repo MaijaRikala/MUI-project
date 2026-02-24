567c1bdbad39d3bb4abb517824b6ea0572248e6c
Maija Rikala <163442056+MaijaRikala@users.noreply.github.com>
2026-02-24 11:21:51 +0200

    added game states to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,7 @@ public class GameManager : MonoBehaviour
 
     // Static private variable to hold the reference
     private static GameManager _instance;
+
     // Public reference for other classes (only get, not set)
     public static GameManager Instance
     {
@@ -19,13 +21,77 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // All possible Gamestates
+    public enum GameState
+    {
+        Play,
+        Options,
+        Victory,
+        Lose
+        // and something for the main menu?
+    }
+
+    // Variable for the current gamestate
+    public GameState State;
+
 
 
     // Initialize ----------------------------------------------------
 
     // Initialize variables, in this case only _instance
     // Set _instance reference as soon as possible
-    private void Awake() => _instance = this;
+    private void Awake() {
+        _instance = this;
+    }
+
+
+    // State control -------------------------------------------------
+
+    // Control gamestate changes
+    public void UpdateGameState(GameState newState)
+    {
+        // Set current state
+        State = newState;
+
+        // Check current state and handle accordingly
+        switch (newState)
+        {
+            case GameState.Play:
+                HandlePlay();
+                break;
+            case GameState.Options:
+                HandleOptions();
+                break;
+            case GameState.Victory:
+                HandleVictory();
+                break;
+            case GameState.Lose:
+                HandleLose();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
+        }
+    }
+
+    private void HandlePlay()
+    {
+        //tähän ehkä SceneManager.LoadScene() ja tonne scenen nimi/indeksi
+    }
+
+    private void HandleOptions()
+    {
+        //tähän ehkä SceneManager.LoadScene() ja tonne scenen nimi/indeksi
+    }
+
+    private void HandleLose()
+    {
+        //tähän ehkä SceneManager.LoadScene() ja tonne scenen nimi/indeksi
+    }
+
+    private void HandleVictory()
+    {
+        //tähän ehkä SceneManager.LoadScene() ja tonne scenen nimi/indeksi
+    }
 
 
 
